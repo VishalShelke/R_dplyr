d1b9281db356587093431f6db9e958465158618e
VishalShelke <vishal.shelke7@gmail.com>
2016-04-08 00:41:07 -0400

    R dplyr filter
    R dplyr filter

---
+++ dplyruses.R
@@ -0,0 +1,14 @@
+rm(list=ls())
+library(quantmod)
+library(dplyr)
+library(magrittr)
+library(datasets)
+head(CO2)
+
+
+d2<-CO2
+d3.a1<-filter(d2, Plant =="Qn1")#select rows
+d3.a2<-filter(d2, Plant %in% c("Qn1","Mn2"))
+d3.a3<-filter(d2, Plant=="Mn1"|Plant=="Mn2")
+d3.a4<-filter(d2, Plant=="Mn1"|Plant=="Qn1",conc>100,conc<600)
+d3.a5<-filter(d2, Plant=="Mn2"|Plant=="Qc2",conc>100,conc<600,uptake<40)
