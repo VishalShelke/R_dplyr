c38fb89ededb46d66b3f6fec0c848f753ed28cc8
VishalShelke <vishal.shelke7@gmail.com>
2016-04-08 02:20:03 -0400

    updated all needed

--- dplyruses.R
+++ dplyruses.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-library(quantmod)
+library(data.table)
 library(dplyr)
 library(magrittr)
 library(datasets)
@@ -20,3 +20,23 @@ d3.a6<-select(d2, Type,conc )#select columns
 d3.a7<-select(d2, contains("up") )
 d3.a8<-select(d2, starts_with("T") )
 d3.a9<-select(d2, ends_with("e") )
+
+
+d3.a10<-arrange(d2, Plant )#order
+d3.a11<-arrange(d2, desc(conc))
+d3.a12<-arrange(d2, desc(conc*uptake))
+
+
+d3.a13<-mutate(d2, weigthed=(conc*uptake))
+d3.a14<-mutate(d2,plantID=paste(Plant, Type, sep = "-"))
+d3.a15<-mutate(d2,variable=ifelse(conc>100,"Positve","Negative"))
+
+k<-d2 %>%
+  filter(Plant %like% "Q") %>% 
+  select(Plant,Type,conc,uptake)%>%
+  group_by(Plant,Type) %>%
+  summarise(sumconc=sum(conc),Meantake=mean(uptake))
+
+
+
+
